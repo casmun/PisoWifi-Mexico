4a74d5a291ef350a972c16a643c4958a39c61fde
alucard9523 <75283067+alucard9523@users.noreply.github.com>
2021-01-12 12:27:53 -0600

    Add files via upload
    Versión con WifiManager

--- 002.ino
+++ 002.ino
@@ -11,6 +11,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <LiquidCrystal_I2C.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
 
 #define CONTROL_PIN 16
 
@@ -18,22 +20,22 @@ ESP8266WebServer server(80);
 WiFiClient client;
 ESP8266telnetClient tc(client);
 
-#ifndef STASSID
-#define STASSID "YourWiFi"  // El Wifi en tu Hotspot
-#define STAPSK  ""          // Contraseña del AP de tu Hotspot, si no la tiene dejalo en blanco
-#endif
+//#ifndef STASSID
+//#define STASSID "**********"  // your Hotspot server
+//#define STAPSK  "**********"          // hotspot password were always open
+//#endif
 
-const char* ssid     = STASSID;
-const char* password = STAPSK;
+//const char* ssid     = STASSID;
+//const char* password = STAPSK;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);  // Cambia esto dependiendo el tipo de Display LCD
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // change this according to your lcd
 
 /* Soft AP network parameters */
 //IPAddress apIP(192, 168, 88, 1);
 //IPAddress netMsk(255, 255, 255, 0);
 
 //put here your raspi ip address, and login details
-IPAddress mikrotikRouterIp (10, 0, 0, 1);  // La IP de tu Hotspot por defecto puede ser 10.5.50.1
+IPAddress mikrotikRouterIp (192, 168, 50, 1);  // should be your mikrotik hotspot IP address
 
 // other variables
 char tmp[16];
@@ -49,6 +51,7 @@ bool buttonWasPressed = false, display_init = false;
 long timer=0;
 byte last_sec = 0, second = 0;
 int multi = 0;
+int profil = 0;
 
 void ICACHE_RAM_ATTR coinInsert(){
   coinCount++;
@@ -63,10 +66,14 @@ void ICACHE_RAM_ATTR coinInsert(){
   //ESP.reset();
 //}
 
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin (115200);
-  attachInterrupt(14, coinInsert, RISING);
+  Serial.begin (9600);
+  print_txt("Configure Wifi...", 0, 0);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("MikroWifi");
+    attachInterrupt(14, coinInsert, RISING);
   pinMode(CONTROL_PIN,OUTPUT);  // D0
   pinMode(2, OUTPUT);     // D2
   pinMode(12, INPUT);     // D6
@@ -79,7 +86,7 @@ void setup() {
   idle();
 
   delay(2000);
-  print_txt("Wait for WiFi...", 0, 0);
+  print_txt("Esperando Wifi...", 0, 0);
   Serial.println();
   Serial.print("Wait for WiFi...");
 
@@ -90,7 +97,7 @@ void setup() {
   Serial.println("Connecting to WiFi network.");
 
   WiFi.mode(WIFI_STA);  // station mode
-  WiFi.begin(ssid, password); // ssid and password
+  //WiFi.begin(ssid, password); // ssid and password
   delay(1000); // pause for a while
   int count = 0;
   lcd.setCursor(0, 1);
@@ -100,10 +107,10 @@ void setup() {
   }
   if (WiFi.status() != WL_CONNECTED){ 
     Serial.println("");
-    Serial.print("Failed to connect to ");
-    Serial.println(ssid);
-    Serial.println("Restarting device");
-    print_txt("NoWiFi,RESETTING", 0, 1);
+    Serial.print("Error de Conexion ");
+    //Serial.println(ssid);
+    Serial.println("Reiniciando");
+    print_txt("No WIFI", 0, 1);
     delay(1000);
     ESP.reset();
   }
@@ -113,11 +120,11 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("");
-  Serial.println("Trying to connect to Mikrotik");
+  Serial.println("Conectando a MikroTik");
 
-  print_txt(" WiFi Connected ", 0, 1);
+  print_txt(" Conectado ", 0, 1);
   delay(1000);
-  print_txt("Connecting to MT", 0, 1);
+  print_txt("Conectando a MK", 0, 1);
 
 
 
@@ -127,22 +134,22 @@ void setup() {
   delay(500);
   
   //PUT HERE YOUR USERNAME/PASSWORD
-  if(tc.login(mikrotikRouterIp, "admin", "admin12345")){        //tc.login(mikrotikRouterIp, "admin", "password", 1234) if you want to specify a port different than 23
+  if(tc.login(mikrotikRouterIp, "esp8266", "230395")){        //tc.login(mikrotikRouterIp, "admin", "password", 1234) if you want to specify a port different than 23
     tc.sendCommand("ip");
     tc.sendCommand("address");
     tc.sendCommand("print");
   }
   else{
-    Serial.println("Mikrotik login ok");
-    print_txt("Connection: OK! ", 0, 1);
+    Serial.println("Mikrotik: OK");
+    print_txt("Conexion:   OK! ", 0, 1);
     delay(1000);
   }
 
   // web interface starts here
   server.begin();
-  Serial.println("Server started");
-  print_txt("Connection: OK! ", 0, 0);
-  print_txt("Server Started  ", 0, 1);
+  Serial.println("Iniciando Sistema");
+  print_txt("Conexion: OK! ", 0, 0);
+  print_txt("Servidor: OK! ", 0, 1);
   delay(2000);
   idle();
   digitalWrite(CONTROL_PIN, LOW);
@@ -167,7 +174,7 @@ void loop() {
         idle();
       }
       else if(!clientToken){
-        print_txt("Insert Coin     ", 0, 0);
+        print_txt("Inserte Moneda", 0, 0);
         print_txt("                ", 0, 1);
         clientToken = true;
         coinCount = 0;
@@ -197,14 +204,51 @@ void loop() {
           startTimer--;
         }
         print_txt(dtostrf((startTimer), 2, 0, tmp), 14, 0);
-        if(coinCount >= 10){                               // rate 1
-          multi = 1000;
+        
+         if(coinCount >= 45){                               // Perfil 10= 45 dias
+          multi = 86400;
+          profil = 10;
         }
-        else if(coinCount >= 5){                           // rate 2
-          multi = 950;
+
+           if(coinCount >= 30){                               // Perfil 9=1 MesX2 Para 2 Usuarios
+          multi = 86400;
+          profil = 9;
+        }
+        
+        else if(coinCount >= 20){                               // Perfil 8=1 Mes Para 1 Usuario
+          multi = 129600;
+          profil = 8;
+        }
+
+        else if(coinCount >= 16){                           // Perfil 7=15 Dias
+          multi = 81000;
+          profil = 7;
+          }
+
+        else if(coinCount >= 10){                           // Perfil 6=7 Dias
+          multi = 60480;
+          profil = 6;
+          }
+        
+        else if(coinCount >= 5){                           // Perfil 5=1 Dia
+          multi = 17280;
+          profil = 5;
+        }
+        else if(coinCount >= 4){                           // Perfil 4=2 Horas
+          multi = 1800;
+          profil = 4;
+        }
+        else if(coinCount >= 3){                           // Perfil 3=1:30Minutos
+          multi = 1800;
+          profil = 3;
+        }
+        else if(coinCount >= 2){                           // Perfil 2=1 Hora
+          multi = 1800;
+          profil = 2;
         }
         else{
-          multi = 900;                                     // rate 3
+          multi = 1800;                                     // Perfil 1=30 Minutos
+          profil = 1;
         }
         credit = coinCount * multi;                        // product of the coin inserted with multiplier
         totalTime = credit;                                // save it to other variable
@@ -213,9 +257,9 @@ void loop() {
         Min = Min % 60;
         Sec = totalTime %60;
         Day = floor(Hr / 24);
-        print_txt("P=", 0, 1);
+        print_txt("C=", 0, 1);
         lcd.print(coinCount);
-        snprintf(tmp, 16, "%02d:%02d:%02d:%02d", Day, Hr, Min, Sec);
+        snprintf(tmp, 16, "%02dd:%02dh:%02dm:%02ds", Day, Hr, Min, Sec);
         print_txt(tmp, 5, 1);
       }
       else{
@@ -257,21 +301,25 @@ void loop() {
 
   void sendMT(int value){
     String data;
-    userName = random(1000, 9999);
+    userName = random(10000, 99990);
     data = "ip hotspot user add name="; // "ip hotspot user add name=username limit-uptime=660 profile=vendo server=hotspot1" -> whole terminal command
     data += userName;
-    data += " limit-uptime=";
-    data += value;
-    data += " profile=vendo";         // comment this out if you dont used user profile in your mt
+    //data += " limit-uptime=";   // Para poner timepo en limit-uptime dejar estas dos opciones limit y value
+    //data += value;
+    data += " password=";
+    data += userName;
+    data += " profile=";         // comment this out if you dont used user profile in your mt
+    data += profil;
     data += " server=hotspot1";       // comment this out if you dont want this server profile to used in your users
     int datalen = data.length() +1;
     char dataSend[datalen];
     data.toCharArray(dataSend, datalen);
     tc.sendCommand(dataSend);
-    print_txt(" <- Your Code -> ", 0, 0);
+    print_txt("<- TU CODIGO -> ", 0, 0);
+    print_txt("PIN=", 0, 1);
     lcd.setCursor(6,1);
     lcd.print(userName);
-    startTimer = 30;
+    startTimer = 60;
     displayCode = true;
     /* script that will remove expired user, put this on your user profile(vendo in my case) script
     #removing user from hotspot
@@ -288,6 +336,9 @@ void loop() {
   }
 
   void idle(){
-    print_txt("   Welcome To   ", 0, 0);
-    print_txt("  Your Hotspot  ", 0, 1);
+    print_txt("   Bienvenido   ", 0, 0);
+    print_txt(" Fichas WebLand  ", 0, 1);
+    
+
+          
   }
